7c3d182aa74fd9a192e55fe452a810a4200e6d7f
Tomas Hubelbauer <tom.hubelbauer@microsoft.com>
2019-02-21 16:15:31 +0100

    Build and publish the application to GitHub Pages

---
+++ docs/precache-manifest.3f403234a04c4f5750f44b706ee0dde8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46c321f7d787b070f768",
+    "url": "/rbc-dnd-auto-select/static/js/runtime~main.46c321f7.js"
+  },
+  {
+    "revision": "a842f9dd6d3bb23151d9",
+    "url": "/rbc-dnd-auto-select/static/js/main.a842f9dd.chunk.js"
+  },
+  {
+    "revision": "2ed8334165d3f344c7b2",
+    "url": "/rbc-dnd-auto-select/static/js/2.2ed83341.chunk.js"
+  },
+  {
+    "revision": "2ed8334165d3f344c7b2",
+    "url": "/rbc-dnd-auto-select/static/css/2.e27f45bb.chunk.css"
+  },
+  {
+    "revision": "ac5119e241819f7e80efe75aa26a9d70",
+    "url": "/rbc-dnd-auto-select/index.html"
+  }
+];
